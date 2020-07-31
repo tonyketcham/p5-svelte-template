e5096e21b5e39a9a14d572adb851c707cf943bc8
tonyketcham <ketcham@pnw.edu>
2020-07-30 22:28:38 -0500

    p5/svelte integration

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,44 @@
 <script>
-	export let name;
+  export let library;
+  import Canvas from './p5/Canvas.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  header {
+    margin-bottom: 3rem;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    color: #ed225d;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <header>
+    <h1>
+      <img src={library.logo} alt={library.name} />
+      /Svelte
+    </h1>
+    <a class="button" href="https://svelte.dev/tutorial">Learn Svelte</a>
+    •
+    <a class="button" href="https://p5js.org/get-started/">Learn p5</a>
+  </header>
+
+  <Canvas />
+</main>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import App from './App.svelte';
+import App from "./App.svelte";
 
 const app = new App({
-	target: document.body,
-	props: {
-		name: 'world'
-	}
+  target: document.body,
+  props: {
+    library: {
+      name: "p5.js",
+      logo: "./p5js.svg",
+    },
+  },
 });
 
 export default app;
